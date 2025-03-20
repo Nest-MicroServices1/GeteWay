6fd6dc6fda113b7650c76f0c2b0b8ea96367e8a0
Jeffer <jefferdcondiza@gmail.com>
2025-03-20 18:16:34 -0500

    update-statusOreder

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,8 +1,18 @@
-import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { ORDER_SERVICE } from 'src/config';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
-import { CreateOrderDto } from './dto';
+import { CreateOrderDto, OrderPaginationDto, StatusDto } from './dto';
 import { catchError } from 'rxjs';
+import { PaginationDto } from 'src/common';
 
 @Controller('orders')
 export class OrdersController {
@@ -16,13 +26,22 @@ export class OrdersController {
   }
 
   @Get()
-  findAllOrders() {
-    return this.ordersClient.send('findAllOrders', {});
+  findAllOrders(@Query() orderPaginationDto: OrderPaginationDto) {
+    return this.ordersClient.send('findAllOrders', orderPaginationDto);
   }
 
   @Get(':id')
   findOneOrder(@Param('id') id: string) {
     return this.ordersClient.send('findOneOrder', { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
+  }
+
+  @Patch(':id')
+  changeStatus(@Param('id') id: string, @Body() statusDto: StatusDto) {
+    return this.ordersClient.send('changeOrdersStatus', { id, status: statusDto.status }).pipe(
       catchError((err) => {
         throw new RpcException(err);
       }),
